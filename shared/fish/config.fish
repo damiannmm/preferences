ff3fd9c3cfd665d59837c57fa241cea6f7cf3827
Damiann Mangan <damiann.mm@gmail.com>
2025-01-03 23:34:06 +0700

    update firefox and fish configs

--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -3,83 +3,83 @@ if status is-interactive
 end
 
 
-## setup PATH
+# setup PATH
 # set __prepend_path foo_1
 # set -a __prepend_path bar_1
 
-# set __append_path foo_2
-# set -a __append_path bar_2
+set __append_path /usr/local/opt/mysql-client/bin
+set -a __append_path /Users/damiann/Library/Python/3.9/bin
 
 # set PATH $__prepend_path $PATH
-# set PATH $PATH $__prepend_path
+set PATH $PATH $__append_path
 
 # set -e __prepend_path
-# set -e __append_path
+set -e __append_path
 
-# variables
-function __prompt_icon
-    set bg_col $argv
-    set_color -b $bg_col
-    echo -n ' $ '
-    set_color $bg_col -b normal
-    echo -n ''
-    set_color normal
-end
-set tide_character_icon (__prompt_icon black)
+# # variables
+# function __prompt_icon
+#     set bg_col $argv
+#     set_color -b $bg_col
+#     echo -n ' $ '
+#     set_color $bg_col -b normal
+#     echo -n ''
+#     set_color normal
+# end
+# set tide_character_icon (__prompt_icon black)
 
-set -x EDITOR 'nvim'
+# set -x EDITOR 'nvim'
 
-set -x GPG_TTY (tty)
+# set -x GPG_TTY (tty)
 
-set -x LC_ALL 'en_US.UTF-8'
-set -x LANG 'en_US.UTF-8'
+# set -x LC_ALL 'en_US.UTF-8'
+# set -x LANG 'en_US.UTF-8'
 
-set -x LS_COLORS 'ow=01;36'
+# set -x LS_COLORS 'ow=01;36'
 
-set -x LESS_TERMCAP_mb \e\[1\x3B32m
-set -x LESS_TERMCAP_md \e\[1\x3B32m
-set -x LESS_TERMCAP_me \e\[0m
-set -x LESS_TERMCAP_se \e\[0m
-set -x LESS_TERMCAP_so \e\[01\x3B33m
-set -x LESS_TERMCAP_ue \e\[0m
-set -x LESS_TERMCAP_us \e\[1\x3B4\x3B31m
+# set -x LESS_TERMCAP_mb \e\[1\x3B32m
+# set -x LESS_TERMCAP_md \e\[1\x3B32m
+# set -x LESS_TERMCAP_me \e\[0m
+# set -x LESS_TERMCAP_se \e\[0m
+# set -x LESS_TERMCAP_so \e\[01\x3B33m
+# set -x LESS_TERMCAP_ue \e\[0m
+# set -x LESS_TERMCAP_us \e\[1\x3B4\x3B31m
 
-set -x unamer (uname -r)
+# set -x unamer (uname -r)
 
 
 # aliases and functions
-switch (uname)
-    case Darwin
-        alias _pwd /bin/pwd
-        alias _ls /bin/ls
-    case Linux
-        alias _pwd /usr/bin/pwd
-        alias _ls '/usr/bin/ls --color'
+# switch (uname)
+#     case Darwin
+#         alias _pwd /bin/pwd
+#         alias _ls /bin/ls
+#     case Linux
+#         alias _pwd /usr/bin/pwd
+#         alias _ls '/usr/bin/ls --color'
 
-        if string match -q '*WSL*' (uname -r)
-            alias pbpaste 'win32yank -o'
-            alias pbcopy 'win32yank -i'
-        end
-end
+#         if string match -q '*WSL*' (uname -r)
+#             alias pbpaste 'win32yank -o'
+#             alias pbcopy 'win32yank -i'
+#         end
+# end
 
-alias ls '_ls -FG'
-alias ll 'ls -hloAF'
-alias la 'ls -A'
+# alias ls '_ls -FG'
+# alias ll 'ls -hloAF'
+# alias la 'ls -A'
 
-alias vi 'nvim'
-alias vim 'nvim'
+# alias vi 'nvim'
+# alias vim 'nvim'
 
-function gxx --argument-names "opt" "filename"
-    g++ $filename
+# function gxx --argument-names "opt" "filename"
+#     g++ $filename
 
-    if [ "$opt" = "r" ]
-        time ./a.out
-    else if [ "$opt" = "i" ]
-        time ./a.out < input.txt
-    else if [ "$opt" = "io" ]
-        time ./a.out < input.txt > output.txt
-    end
-end
+#     if [ "$opt" = "r" ]
+#         time ./a.out
+#     else if [ "$opt" = "i" ]
+#         time ./a.out < input.txt
+#     else if [ "$opt" = "io" ]
+#         time ./a.out < input.txt > output.txt
+#     end
+# end
 
 function __cd
     set tmpdir $dirprev
@@ -89,32 +89,32 @@ function __cd
     else
         builtin cd $argv
     end
-    if [ (_ls | wc -l) -le 16 ]
+    if [ (command ls | wc -l) -le 16 ]
         ls
     end
 end
 alias cd '__cd'
 
 function __pwd
-    _pwd -L
-    if [ (_pwd -L) != (_pwd -P) ]
-        _pwd -P
+    builtin pwd -L
+    if [ (builtin pwd -L) != (builtin pwd -P) ]
+        builtin pwd -P
     end
 end
 alias pwd '__pwd'
 
 function __git
     if [ "$argv" = "log" ]
-        hub $argv --all --decorate --oneline --graph
+        command git $argv --all --decorate --oneline --graph
     else
-        hub $argv
+        command git $argv
     end
 end
 alias git '__git'
 
 
-# bindings
-bind ! __history_previous_command
-bind '$' __history_previous_command_arguments
-bind -k nul complete
-bind \t accept-autosuggestion
+# # bindings
+# bind ! __history_previous_command
+# bind '$' __history_previous_command_arguments
+# bind -k nul complete
+# bind \t accept-autosuggestion
